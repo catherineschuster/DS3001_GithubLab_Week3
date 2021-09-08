c070727dc612be48ec8b38cc0da051c0cdbb54c2
Kate <kcd6znz@virginia.edu>
2021-09-08 15:46:00 -0400

    commit

--- Graph_1.R
+++ Graph_1.R
@@ -1,4 +1,9 @@
 Pregnancy_data <- 
   read.csv("/Users/catherineschuster/Desktop/Fall 2021/DS 3001/DS-3001/data/pregnancy.csv")
 
+#creating bar graph
+
+library(ggplot2) 
+ggplot(Pregnancy_data, aes(x=Cigarettes, y=Wine))
+geom_bar()
 
